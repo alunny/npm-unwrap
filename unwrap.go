7dc945c98a846a91c95b6b58f73e864ff9c10400
alunny <alunny@gmail.com>
2016-01-02 13:00:19 -0800

    display version when asked

--- unwrap.go
+++ unwrap.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/alunny/npm-unwrap/npm"
 )
 
-func main() {
+const Version = "0.0.1"
+
+func install() {
 	shrinkwrapReader, err := os.Open("./npm-shrinkwrap.json")
 	if err != nil {
 		log.Fatal("could not read ./npm-shrinkwrap.json")
@@ -23,4 +26,19 @@ func main() {
 	npm.PrintApp(app)
 
 	app.Install()
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		cmd := os.Args[1]
+
+		if cmd == "version" {
+			fmt.Printf("%s\n", Version)
+			os.Exit(0)
+		} else {
+			log.Fatalf("unrecognized command: %s\n", cmd)
+		}
+	}
+
+	install()
 }
