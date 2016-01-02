5aac72e92b3b010c1c3f76f8403521e15f45d844
alunny <alunny@gmail.com>
2016-01-02 13:00:49 -0800

    download to temp directory

--- npm/download.go
+++ npm/download.go
@@ -2,50 +2,69 @@ package npm
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
 	"sync"
 )
 
 func (a *App) Install() {
 	var wg sync.WaitGroup
 	client := &http.Client{}
+	// can use ioutil for this instead
+	tmpdir := filepath.Join(os.TempDir(), a.Name)
+	os.Mkdir(tmpdir, 0755)
 
 	for _, dep := range a.Dependencies {
 		wg.Add(1)
 		go func(module Module) {
 			defer wg.Done()
-			module.Install(client)
+			module.Install(client, tmpdir)
 		}(dep)
 	}
 
 	wg.Wait()
 
-	fmt.Printf("downloaded %s\n", a.Name)
+	fmt.Printf("downloaded %s to %s\n", a.Name, tmpdir)
 }
 
-func (m *Module) Install(client *http.Client) {
+func (m *Module) Install(client *http.Client, directory string) {
 	var wg sync.WaitGroup
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		m.Download(client)
+		m.Download(client, directory)
 	}()
 
 	for _, dep := range m.Dependencies {
 		wg.Add(1)
 		go func(module Module) {
 			defer wg.Done()
-			module.Install(client)
+			module.Install(client, directory)
 		}(dep)
 	}
 
 	wg.Wait()
 }
 
-func (m *Module) Download(client *http.Client) {
+func (m *Module) Download(client *http.Client, directory string) {
 	fmt.Printf("Downloading %s from %s\n", m.Name, m.Resolved)
 	resp, _ := client.Get(m.Resolved)
+	defer resp.Body.Close()
+
+	target := filepath.Join(directory, path.Base(m.Resolved))
+	output, err := os.Create(target)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer output.Close()
+
+	fmt.Printf("Writing %s\n", target)
+	io.Copy(output, resp.Body)
 
 	/*
 		if err != nil { }
