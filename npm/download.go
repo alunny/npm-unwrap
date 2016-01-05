87b97b8218d3b84a89f4550205cfddcb22f569e5
Andrew Lunny <alunny@twitter.com>
2016-01-04 19:40:14 -0800

    moving download manager to channels

--- npm/download.go
+++ npm/download.go
@@ -11,40 +11,73 @@ import (
 	"sync"
 )
 
+const MaxConcurrentDownloads = 20
+
+type Download struct {
+	module			*Module
+	resultChan	chan int
+}
+
+//var downloadChannel = make(chan *Download, MaxConcurrentDownloads)
+
 func (a *App) Install() {
 	var wg sync.WaitGroup
 	client := &http.Client{}
+
+	downloadChannel := make(chan *Download, MaxConcurrentDownloads)
+	quitChannel := make(chan bool)
+
 	// can use ioutil for this instead
 	tmpdir := filepath.Join(os.TempDir(), a.Name)
 	os.Mkdir(tmpdir, 0755)
+
+	go processDownloads(downloadChannel, quitChannel, client, tmpdir)
 
 	for _, dep := range a.Dependencies {
 		wg.Add(1)
 		go func(module Module) {
 			defer wg.Done()
-			module.Install(client, tmpdir)
+			module.Install(client, downloadChannel)
 		}(dep)
 	}
 
 	wg.Wait()
+	quitChannel <- true
 
 	fmt.Printf("downloaded %s to %s\n", a.Name, tmpdir)
 }
 
-func (m *Module) Install(client *http.Client, directory string) {
+// goroutine to handle incoming downloads
+func processDownloads(downloads chan *Download, quit chan bool, client *http.Client, tmpdir string) {
+	for range downloads {
+		download := <-downloads
+		go func(dl Download) {
+			dl.module.Download(client, tmpdir)
+			dl.resultChan <- 1
+		}(*download)
+	}
+	<-quit
+}
+
+func (m *Module) Install(client *http.Client, downloadChannel chan *Download) {
 	var wg sync.WaitGroup
 
 	wg.Add(1)
+
 	go func() {
 		defer wg.Done()
-		m.Download(client, directory)
+		downloadResult := make(chan int)
+		downloadChannel <- &Download{m, downloadResult}
+		<-downloadResult
+		fmt.Printf("done with %s\n", m.Name)
 	}()
 
 	for _, dep := range m.Dependencies {
 		wg.Add(1)
 		go func(module Module) {
 			defer wg.Done()
-			module.Install(client, directory)
+			module.Install(client, downloadChannel)
+			fmt.Printf("done with %s\n", module.Name)
 		}(dep)
 	}
 
@@ -52,8 +85,13 @@ func (m *Module) Install(client *http.Client, directory string) {
 }
 
 func (m *Module) Download(client *http.Client, directory string) {
-	fmt.Printf("Downloading %s from %s\n", m.Name, m.Resolved)
-	resp, _ := client.Get(m.Resolved)
+	// fmt.Printf("Downloading %s from %s\n", m.Name, m.Resolved)
+
+	resp, err := client.Get(m.Resolved)
+	if err != nil {
+		fmt.Printf("Error downloading %s from '%s'\n", m.Name, m.Resolved)
+		return
+	}
 	defer resp.Body.Close()
 
 	target := filepath.Join(directory, path.Base(m.Resolved))
@@ -63,12 +101,12 @@ func (m *Module) Download(client *http.Client, directory string) {
 	}
 	defer output.Close()
 
-	fmt.Printf("Writing %s\n", target)
+	// fmt.Printf("Writing %s\n", target)
 	io.Copy(output, resp.Body)
 
 	/*
 		if err != nil { }
 	*/
 
-	fmt.Printf("Completed downloading %s with status '%s'\n", m.Name, resp.Status)
+	// fmt.Printf("Completed downloading %s\n", m.Name)//, resp.Status)
 }
